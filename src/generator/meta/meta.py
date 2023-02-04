3c55daa1bdd265d63b88edf55cdb1953774c523b
Eynas <xpavel39@vutbr.cz>
2023-02-04 12:35:40 +0100

    implemented inheritance for meta.vectors

--- src/generator/meta/meta.py
+++ src/generator/meta/meta.py
@@ -3,13 +3,10 @@ from typing import Callable
 
 """ This file contains parent classes for simple classes used in the '/model' directory. """
 
-
-
-class FloatVector:
-    """ Class represents a TFLite vector of float values. Provides interface for storing data
+class TFLiteAtomicVector:
+    """ Represents a TFLite vector of atomic values. Provides interface for storing data
         and generating output TFLite code. """
-
-    floatList: list[float]
+    vector: list[int|float|bool] # Item type has to be atomic!
 
     """ TFLite 'Start...Vector' function for the exact vector. Takes 2 arguments, 
     'floatbuffers.Builder' and number of list elements """
@@ -19,77 +16,42 @@ class FloatVector:
     as argument """
     PrependFunction: Callable[[fb.Builder],None]
 
-    def __init__(self, floatList: list[float], StartFunction: Callable[[fb.Builder, int],None]
-    , PrependFunction: Callable[[fb.Builder],None] = lambda builder: builder.PrependFloat32) -> None:
-        self.floatList = floatList
+    def __init__(self, list: list, StartFunction: Callable[[fb.Builder, int],None]
+                , PrependFunction: Callable[[fb.Builder],None]) -> None:
+        self.list = list
         self.StartFunction = StartFunction
         self.PrependFunction = PrependFunction
 
     def genTFLite(self, builder: fb.Builder):
         """ Generates TFLite code for the vector """
-        self.StartFunction(builder, len(self.floatList))
+        self.StartFunction(builder, len(self.list))
 
-        for floatVal in self.floatList:
-            self.PrependFunction(builder)(floatVal)
+        for val in self.list:
+            self.PrependFunction(builder)(val)
 
         return builder.EndVector()
 
+class FloatVector(TFLiteAtomicVector):
+    """ Class represents a TFLite vector of float values. Provides interface for storing data
+        and generating output TFLite code. """
+
+    def __init__(self, floatList: list[float], StartFunction: Callable[[fb.Builder, int],None]
+    , PrependFunction: Callable[[fb.Builder],None] = lambda builder: builder.PrependFloat32) -> None:
+        super().__init__(floatList,StartFunction,PrependFunction)
 
 
-class IntVector:
+
+class IntVector(TFLiteAtomicVector):
     """ Class represents a TFLite vector of integer values. Provides interface for storing data
         and generating output TFLite code. """
 
-    intList: list[int]
-
-    """ TFLite 'Start...Vector' function for the exact vector. Takes 2 arguments,
-    'floatbuffers.Builder' and number of list elements """
-    StartFunction: Callable[[fb.Builder, int],None] 
-    
-    """ TFLite 'Prepend...' function for the exact vector item type. Takes 'flatbuffers.Builder' 
-    as argument """
-    PrependFunction: Callable[[fb.Builder],None] 
-
     def __init__(self, intList: list[int], StartFunction: Callable[[fb.Builder, int],None]
     , PrependFunction: Callable[[fb.Builder],None] = lambda builder: builder.PrependInt32) -> None:
-        self.intList = intList
-        self.StartFunction = StartFunction
-        self.PrependFunction = PrependFunction
+        super().__init__(intList,StartFunction,PrependFunction)
 
-    def genTFLite(self, builder: fb.Builder):
-        """ Generates TFLite code for the vector """
-        self.StartFunction(builder, len(self.intList))
-
-        for intVal in self.intList:
-            self.PrependFunction(builder)(intVal)
-
-        return builder.EndVector()
-
-class BoolVector:
+class BoolVector(TFLiteAtomicVector):
     """ Class represents a TFLite vector of boolean values. Provides interface for storing data
         and generating output TFLite code. """
-
-    boolList: list[bool]
-
-    """ TFLite 'Start...Vector' function for the exact vector. Takes 2 arguments,
-    'floatbuffers.Builder' and number of list elements """
-    StartFunction: Callable[[fb.Builder, int],None] 
-    
-    """ TFLite 'Prepend...' function for the exact vector item type. Takes 'flatbuffers.Builder' 
-    as argument """
-    PrependFunction: Callable[[fb.Builder],None] 
-
     def __init__(self, boolList: list[bool], StartFunction: Callable[[fb.Builder, int],None]
     , PrependFunction: Callable[[fb.Builder],None] = lambda builder: builder.PrependBool) -> None:
-        self.boolList = boolList
-        self.StartFunction = StartFunction
-        self.PrependFunction = PrependFunction
-
-    def genTFLite(self, builder: fb.Builder):
-        """ Generates TFLite code for the vector """
-        self.StartFunction(builder, len(self.boolList))
-
-        for boolVal in self.boolList:
-            self.PrependFunction(builder)(boolVal)
-
-        return builder.EndVector()
+        super().__init__(boolList,StartFunction,PrependFunction)
