246888c1d9e5d60b5bbaf072009e67b9fdaa98d7
Eynas <xpavel39@vutbr.cz>
2023-02-18 13:06:50 +0100

    moved the '/infer/' dir to 'generator_test.py'

--- generator_test.py
+++ generator_test.py
@@ -19,8 +19,6 @@ import src.generator.builtin.MaxPool2D as MaxPool2D
 import src.generator.builtin.FullyConnected as FullyConnected
 import src.generator.builtin.Softmax as Softmax
 
-import src.infer.main as infer
-
 """ Generate a basic .tflite model. 
 Should behave identically to the '/data/cifar10/cifar10_model.tflite'.
 """
@@ -237,4 +235,48 @@ with open("test/out.tflite","wb") as f:
     f.write(buffer)
 
 
-infer.runTest()
+""" Compare the output of generated model with the pre-trained model. """
+
+
+import tensorflow as tf
+
+from PIL import Image
+
+def loadModels():
+    generatedModel = tf.lite.Interpreter(model_path = "./test/out.tflite")
+    generatedModel.allocate_tensors()
+
+    premadeModel = tf.lite.Interpreter(model_path = "./data/cifar10/cifar10_model.tflite")
+    premadeModel.allocate_tensors()
+
+    return (generatedModel,premadeModel)
+
+def loadData():
+    image = Image.open("./data/cifar10/airplane1.png")
+    image = [np.asarray(image).tolist()]
+    image = np.asarray(image,np.uint8)
+
+    return image
+
+
+def classify(model, image):
+    # Get input and output tensors.
+    input_details = model.get_input_details()
+    output_details = model.get_output_details()
+
+    model.set_tensor(input_details[0]['index'], image)
+
+    model.invoke()
+
+    output_data = model.get_tensor(output_details[0]['index'])
+    print(output_data)
+
+
+def runTest():
+    generatedModel, premadeModel = loadModels()
+    image = loadData()
+
+    classify(generatedModel, image)
+    classify(premadeModel, image)
+
+runTest()
