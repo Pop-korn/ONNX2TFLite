105f0ad3010a116c0a07001b7ef4cadbd90bd81a
Eynas <xpavel39@vutbr.cz>
2023-02-22 21:29:45 +0100

    Conv2D conversion done

--- src/convertor/conversion/OperatorConverter.py
+++ src/convertor/conversion/OperatorConverter.py
@@ -15,13 +15,27 @@ import src.err as err
 """ -------------------- Helper Operator Functions -------------------- """
 
 
-def __convertPadding(oPads: list[int]) -> tflPad.Padding:
-    """ Convert ONNX pads to TFLite padding. """
+def __convertPadding(autoPad: str, oPads: list[int]) -> tflPad.Padding:
+    """ Convert ONNX pads to TFLite padding. 
+        'autoPad' is the ONNX attribute 'auto_pad' and 'oPads' is the ONNX attribute 'pads'. """
+
+    if autoPad == "SAME_UPPER":
+        return tflPad.Padding.SAME
+
+    elif autoPad == "SAME_LOWER":
+        err.note(f"TFLite does NOT support 'SAME_LOWER' padding! Using 'SAME', which is equivalent to 'SAME_UPPER'.")
+        return tflPad.Padding.SAME
+
+    elif autoPad == "VALID":
+        return tflPad.Padding.VALID
+
+    # autoPad is NOTSET -> use explicit padding
+
     if all(val == 0 for val in oPads):
         # No padding in any dieraction
         return tflPad.Padding.VALID
 
-    err.note(f"TFLite does NOT support '{oPads}' padding! Using 'SAME', i.e. use as much padding as needed.")
+    err.note(f"TFLite does NOT support '{oPads}' padding! Using 'SAME'.")
     return tflPad.Padding.SAME
 
 
@@ -41,20 +55,29 @@ def convertNode(oNode: onnxN.Node, tensorIndexForName: Callable[[str],int]) -> t
 
     return tOp
 
+
 def convertConv(oConv: onnxConv.Conv) -> tflConv2D.Conv2D:
-    """ Convert the  """
-    tConv = tflConv2D.Conv2D()
+    """ Convert the ONNX 'Conv' operator to TFLite """
 
-    if len(oConv.strides) == 2:
-        tConv.strideH = oConv.strides[0]
-        tConv.strideW = oConv.strides[1]
+    match len(oConv.kernelShape):
+        case 2:
+            tConv = tflConv2D.Conv2D()
 
-    if oConv.dilations is not None and len(oConv.dilations) == 2:
-        tConv.dilationHFactor = oConv.dilations[0]
-        tConv.dilationHFactor = oConv.dilations[1]
+            if len(oConv.strides) == 2:
+                tConv.strideH = oConv.strides[0]
+                tConv.strideW = oConv.strides[1]
 
-    tConv.padding = __convertPadding(oConv.pads)
+            if oConv.dilations is not None and len(oConv.dilations) == 2:
+                tConv.dilationHFactor = oConv.dilations[0]
+                tConv.dilationHFactor = oConv.dilations[1]
 
-    print(tConv.builtinOptionsType)
+            tConv.padding = __convertPadding(oConv.autoPad, oConv.pads)
 
-    return tConv
+            # TODO tConv.fusedActivationFunction
+
+            return tConv
+
+        case 3:
+            err.error("Conv3D NEEDS to be implemented and converted!")
+        case _:
+            err.error(f"Convolution with kernel shape '{oConv.kernelShape}' is not supported!")
