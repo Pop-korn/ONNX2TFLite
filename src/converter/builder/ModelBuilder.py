1872f1c24cd7fef76f90615e38363f59c8fe9b91
Martin Pavella <xpavel39@stud.fit.vutbr.cz>
2023-03-21 21:51:36 +0100

    Fix mistake

--- src/converter/builder/ModelBuilder.py
+++ src/converter/builder/ModelBuilder.py
@@ -198,9 +198,6 @@ class ModelBuilder:
         # Fuse Reshape operators if possible
         self.__fuseReshapeOperators()
 
-        # Fuse Reshape operators if possible
-        self.__fuseReshapeOperators()
-
         # Remove unused tensors and bufers
         self.__removeUnusedTensorsAndBuffers()
 
