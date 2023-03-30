68afe235a4674e8dd28442e4cb864f923b2db5b8
Martin Pavella <xpavel39@stud.fit.vutbr.cz>
2023-03-30 16:32:07 +0200

    rename cvt_test.py, add strict test for model conversion, add comments to convesion_test.py

--- conversion_test.py
+++ conversion_test.py
@@ -14,19 +14,33 @@ err.MIN_OUTPUT_IMPORTANCE = err.MessageImportance.LOWEST
 
 
 def printStats(formatName, output):
+    """ Print some statistical information about the given numpy arrray. """
+
+    # Find the two highest values
+    secMax = 0
+    max = 0
+    for el in output.flatten():
+        if el >= max:
+            secMax = max
+            max = el
+
     print(formatName)
     print(f"\tMax: output[{output.argmax()}] = {output.max()}")
+    print(f"\tSecond Max: = {secMax}")
     print(f"\tShape = {output.shape}")
     print(f"\tSum = {output.sum()}")
     print(f"\tMean = {output.mean()}")
     print(f"\tStd = {output.std()}")
 
 def tensorToNCHW(tensor: np.ndarray):
+    """ Determine if given 'tensor' is in the NCHW format. """
     if len(tensor.shape) < 4:
         return tensor
     return np.moveaxis(tensor,3,1)
 
 def shapeToNHWC(shape):
+    """ Convert NCHW shape to NHWC and return it. If 'shape' is not in NCHW, 
+        nothing is done to it. """
     if len(shape) < 4:
         return shape
     c = shape[1]
@@ -35,11 +49,14 @@ def shapeToNHWC(shape):
     return shape
 
 def loadImage(file):
+    """ Load image from 'file' and return it. """
     img = Image.open(file)
     img = [np.asarray(img).tolist()]
     return np.asarray(img, np.float32)
 
 def runOnnxModel(modelFile, inpt):
+    """ Run ONNX model stored in 'modelFile' with 'inpt' as input. 
+        Return the output tensor. """
     model = onnx.load(modelFile)
     onnx.checker.check_model(model)
 
@@ -49,14 +66,16 @@ def runOnnxModel(modelFile, inpt):
     return np.asarray(res).squeeze()
 
 def runTFLiteModel(modelFile, inpt):
+    """ Run TFLite model stored in 'modelFile' with 'inpt' as input. 
+        Return the output tensor. """
     tflModel = tf.lite.Interpreter(model_path=modelFile)
     tflModel.allocate_tensors()
 
     inDet = tflModel.get_input_details()
     outDet = tflModel.get_output_details()
 
-    print("In:",inDet[0]['shape'])
-    print("Out:",outDet[0]['shape'])
+    # print("In:",inDet[0]['shape'])
+    # print("Out:",outDet[0]['shape'])
 
     tflModel.set_tensor(inDet[0]['index'], inpt)
 
@@ -66,6 +85,9 @@ def runTFLiteModel(modelFile, inpt):
 
 
 def keepOnlyTensors(col, tensorNames):
+    """ Remove all tensors from collection 'col' that don't have names in
+        'tensorNames'. 'col' is expected to be a ONNX 'repeated' attribute and
+        items from it cannot be removed easily. """
     toKeep = []
     for t in col:
         if t.name in tensorNames:
@@ -76,6 +98,8 @@ def keepOnlyTensors(col, tensorNames):
         col.append(t)
 
 def createReducedOnnxModelFrom(fromModel, newModel, lastNode):
+    """ Load ONNX model from 'fromModel', reduce is so it only contains nodes 
+        upto 'lastNode'. Stroe the reduced model in 'newModel' file. """
     model = onnx.load(fromModel)
 
     for vi in model.graph.value_info:
@@ -105,6 +129,11 @@ def createReducedOnnxModelFrom(fromModel, newModel, lastNode):
 def runAndTestFirstNOperators(originalOnnxFile, outOnnxFile, 
                              outTfliteFile, numOpsToPreserve,
                              imageFile):
+    """ Convert the ONNX model in 'originalOnnxFile'. Reduce it to only contain
+        the first 'numOpsToPreserve' operators and save the model in 
+        'outOnnxFile'. Convert the reduced model to TFLite and save it in
+        'outTfliteFile'. Then run both models with the 'imageFile' as input
+        and print output statistics. """
     
     image = loadImage(imageFile)
 
@@ -114,13 +143,18 @@ def runAndTestFirstNOperators(originalOnnxFile, outOnnxFile,
 
     onnxOut = runOnnxModel(outOnnxFile, image)
 
-    tflOut = runTFLiteModel(outTfliteFile, image)
+    tflOut = tensorToNCHW(runTFLiteModel(outTfliteFile, image))
 
     printStats("ONNX", onnxOut)
-    printStats("TFLite", tensorToNCHW(tflOut))
+    printStats("TFLite", tflOut)
+    printStats("Difference", onnxOut-tflOut)
 
 
 def pickOutOperators(onnxFile, startIdx, endIdx):
+    """ Load ONNX model in 'onnxFile'. Keep only its operators with indices from
+        'starIdx' to 'endIdx' and only the used tensors. 
+        Return the reduced model. """
+    
     model = onnx.load(onnxFile)
 
     inputs = []
@@ -133,6 +167,7 @@ def pickOutOperators(onnxFile, startIdx, endIdx):
 
     nodesToRemove = []
 
+    # Remove unwanted operators
     for i, node in enumerate(model.graph.node):
         if i == startIdx:
             inputs = [inpt for inpt in node.input]
@@ -150,9 +185,12 @@ def pickOutOperators(onnxFile, startIdx, endIdx):
         model.graph.node.remove(node)
 
 
+    # Remove unused tensors
     keepOnlyTensors(model.graph.initializer, tensorsToKeep)
     keepOnlyTensors(model.graph.value_info, tensorsToKeep)
 
+
+    # Find the model inputs and outputs
     for vi in model.graph.value_info:
         if vi.name in inputs:
             inputsVI.append(vi)
@@ -172,6 +210,7 @@ def pickOutOperators(onnxFile, startIdx, endIdx):
             o.type.tensor_type.shape.Clear()
             o.name = outputs[0]
 
+    # Assign new model inputs and outputs
     while len(model.graph.input) > 0:
         model.graph.input.pop()
     while len(model.graph.output) > 0:
@@ -186,8 +225,12 @@ def pickOutOperators(onnxFile, startIdx, endIdx):
 
 def runAndTestOperators(originalOnnxFile, outOnnxFile, 
                          outTfliteFile, startIdx, endIdx):
+    """ Take the ONNX model in 'originalOnnxFile'. Reduce it to only contain
+        operators with indices 'startIdx' to 'ednIdx' (both included). Save the 
+        reduced model in 'outOnnxFile' and convert it to TFLite in 
+        'outTfliteFile'. Then Run both reduced models with the same random
+        input data and print statistics. """
     
-    print(originalOnnxFile,outOnnxFile,outTfliteFile)
     onnxModel = pickOutOperators(originalOnnxFile,startIdx, endIdx)          
 
     print(f"\tTesting from '{onnxModel.graph.node[0].op_type}' to",
@@ -195,18 +238,96 @@ def runAndTestOperators(originalOnnxFile, outOnnxFile,
     onnx.save(onnxModel, outOnnxFile)
     convert.convertModel(outOnnxFile, outTfliteFile)
 
+    # Get input shape
     shape = [dim.dim_value if dim.dim_value != 0 else 1 for dim in onnxModel.graph.input[0].type.tensor_type.shape.dim]
-    
     shape = shapeToNHWC(shape)
 
+    # Generate random input
     inpt: np.ndarray = np.random.rand(*shape).astype(np.float32)
 
-
+    # Run the models and print stats
     onnxOut = runOnnxModel(outOnnxFile, inpt)
     printStats("ONNX", onnxOut)
 
-    tflOut = runTFLiteModel(outTfliteFile, inpt)
-    printStats("TFLite", tensorToNCHW(tflOut))
+    tflOut = tensorToNCHW(runTFLiteModel(outTfliteFile, inpt))
+    printStats("TFLite", tflOut)
+
+    diff = onnxOut - tflOut
+    printStats("Difference", diff)
+
+
+def testConversion(onnxFile, tflFile, numIterations):
+    """ Convert ONNX model in 'onnxFile' to TFLite and save it in 'tflFile'. 
+        Then run both models with the same random data 'numIterations' times'.
+        Finally print statistics of the outputs. """
+    
+    onnxModel = onnx.load(onnxFile)
+
+    convert.convertModel(onnxFile, tflFile)
+
+    # Get input shape
+    shape = [dim.dim_value if dim.dim_value != 0 else 1 for dim in onnxModel.graph.input[0].type.tensor_type.shape.dim]
+    shape = shapeToNHWC(shape)
+
+    # Run models with random inputs and collect statistics
+    absErr = []
+    precErr = []
+    meanErr = []
+    meanPercErr = []
+    stdErr = []
+    stdPercErr = []
+    for i in range(numIterations):
+        inpt: np.ndarray = np.random.rand(*shape).astype(np.float32)
+
+        oOut = runOnnxModel(onnxFile, inpt)
+        tOut = tensorToNCHW(runTFLiteModel(tflFile, inpt))
+
+        tmpAbsErr = np.abs(oOut - tOut)
+        tmpPercErr = tmpAbsErr / np.abs(oOut)
+
+        absErr.append(tmpAbsErr)
+        precErr.append(tmpPercErr)
+
+        tmpMeanErr = np.abs(oOut.mean() - tOut.mean())
+        meanErr.append(tmpMeanErr)
+        meanPercErr.append(tmpMeanErr / np.abs(oOut.mean()))
+
+        tmpStdErr = np.abs(oOut.std() - tOut.std())
+        stdErr.append(tmpStdErr)
+        stdPercErr.append(tmpStdErr / np.abs(oOut.std()))
+
+
+    # Calculate the mean and max absolute error
+    maxSum = 0.0
+    meanSum = 0.0
+    for el in absErr:
+        maxSum += el.max()
+        meanSum += el.mean()
+    print("Average MAX ABS ERR =",maxSum/numIterations)
+    print("Average ABS ERR =",meanSum/numIterations)
+
+    # Calculate the mean and max percentage error
+    maxSum = 0.0
+    meanSum = 0.0
+    for el in precErr:
+        maxSum += el.max()
+        meanSum += el.mean()
+    print("Average MAX PERC ERR =",maxSum/numIterations)
+    print("Average PERC ERR =",meanSum/numIterations)
+
+    # Calculate statistics
+    print("Max error of output mean =", np.asarray(meanErr).max())
+    print("Average error of output mean =", np.asarray(meanErr).mean())
+
+    print("Max percentage error of output mean =", np.asarray(meanPercErr).max())
+    print("Average precentage error of output mean =", np.asarray(meanPercErr).mean())
+
+    print("Max error of output std =", np.asarray(stdErr).max())
+    print("Average error of output std =", np.asarray(stdErr).mean())
+
+    print("Max percentage error of output std =", np.asarray(stdPercErr).max())
+    print("Average precentage error of output std =", np.asarray(stdPercErr).mean())
+
 
 
 
@@ -216,6 +337,15 @@ imageFile = "data/224x224/cat2.jpg"
 onnxFile = "data/onnx/ResNet101-DUC-12.onnx"
 onnxReducedFile = "test/duc.onnx"
 tflReducedFile = "test/duc.tflite"
+
+alexnetOnnx = "data/onnx/bvlcalexnet-12.onnx"
+alexnetTfl = "test/alexnet.tflite"
+
+ducOnnx = "data/onnx/ResNet101-DUC-12.onnx"
+ducTfl = "test/duc.tflite"
+
+tinyyoloOnnx = "data/onnx/tinyyolov2-8.onnx"
+tinyyoloTfl = "test/tinyyolo.tflite"
 
 # # TEST ALEXNET CONVERSION
 # runAndTestFirstNOperators("data/onnx/bvlcalexnet-12.onnx","test/alexnet.onnx",
@@ -233,4 +363,6 @@ tflReducedFile = "test/duc.tflite"
 # exit()
 
 
-runAndTestOperators(onnxFile, onnxReducedFile, tflReducedFile,0,100)
+testConversion(alexnetOnnx, alexnetTfl, 10)
+
+# runAndTestOperators(onnxFile, onnxReducedFile, tflReducedFile,354,354)
