4c2f61a30089e9e7925196c504329df26d380d73
Martin Pavella <xpavel39@stud.fit.vutbr.cz>
2023-04-29 14:48:55 +0200

    improve program argument parsing

--- onnx2tflite.py
+++ onnx2tflite.py
@@ -3,15 +3,43 @@ import src.err as err
 
 import sys
 
-if len(sys.argv) < 3:
-    err.error(err.Code.INPUT_ERR, "You need to specify the imput .onnx file and",
-              "the output .tflite file.",
-              "Example use: 'python onnx2tflite model.onnx out/model.tflite'.")
+import argparse
+import ntpath
 
 
-if len(sys.argv) >= 4:
-    errLevel = int(sys.argv[3])
-    err.MIN_OUTPUT_IMPORTANCE = err.MessageImportance(errLevel)
+""" Create argument parser """
+parser = argparse.ArgumentParser(
+    prog = "onnx2tflite",
+    description = "Convert a '.onnx' DNN model to an equivalent '.tflite' model.",
+    usage="python onnx2tflite <input_file.onnx> [-o/--output <output_file.tflite> --verbose]",
+    epilog = "Koniec"
+)
+
+parser.add_argument("onnxFile")
+parser.add_argument("-o", "--output", type=str, required=True, metavar="output_tflite_file")
+parser.add_argument("--verbose", action="store_true")
 
 
-convert.convertModel(sys.argv[1], sys.argv[2])
+""" Parse program arguments """
+args = parser.parse_args()
+inputOnnxFile = args.onnxFile
+outputTFLiteFile = args.output
+
+if outputTFLiteFile is None:
+    # Default output file has the same name as input, but with a different file 
+    # extension and is in the current working directory
+
+    fileName = ntpath.basename(inputOnnxFile) # Get filename
+    fileName = ntpath.splitext(fileName)[0] # Remove '.onnx' extension
+    outputTFLiteFile = fileName + ".tflite"
+
+if args.verbose:
+    # Print all logging messages
+    err.MIN_OUTPUT_IMPORTANCE = err.MessageImportance.LOWEST
+
+
+
+""" Convert the model """
+convert.convertModel(inputOnnxFile, outputTFLiteFile)
+
+print(inputOnnxFile, outputTFLiteFile)
